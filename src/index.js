0864c9b9674d456e636c2ea3e627e0efc983e1fc
Tato Panchulidze <tpanchulidze@unisens.ge>
2023-01-09 20:21:34 +0400

    linear time

--- src/index.js
+++ src/index.js
@@ -1,34 +1,52 @@
 const axios = require("axios");
+const token = "zp4YJam58EiymRe9uRSbJQGzg";
 
-const getAllProducts = async () => {
+const findMinimal = (price1, price2) => {
+  price1 = parseFloat(price1.replace("$", ""));
+  price2 = parseFloat(price2.replace("$", ""));
+
+  return price1 < price2;
+};
+
+const getCheapestProducts = async () => {
   const response = await axios.get(
     "https://api.apify.com/v2/datasets/VuFwckCdhVhoLJJ08/items?clean=true&format=json"
   );
 
   const products = response.data;
-  console.log(products)
   const productsByIds = {};
 
   products.forEach((product) => {
     if (productsByIds[product.productId]) {
-      productsByIds[product.productId].push(product);
+      if (findMinimal(product.price, productsByIds[product.productId].price))
+        productsByIds[product.productId] = product;
     } else {
-      productsByIds[product.productId] = [product];
+      productsByIds[product.productId] = product;
     }
   });
 
-  Object.values(productsByIds).map((products) =>
-    products.sort((a, b) => parseFloat(a["price"].replace('$', '')) - parseFloat(b["price"].replace('$', '')))
-  );
-
-  const cheapests = Object.values(productsByIds).map((products) => products[0]);
-  console.log(cheapests);
-
-  const {data} = await axios.post('https://api.apify.com/v2/datasets?token=zp4YJam58EiymRe9uRSbJQGzg')
-  await axios.post(`https://api.apify.com/v2/datasets/${data.data.id}/items?token=zp4YJam58EiymRe9uRSbJQGzg`, cheapests)
-
-  const newResponse = await axios.get(`https://api.apify.com/v2/datasets/${data.data.id}/items?token=zp4YJam58EiymRe9uRSbJQGzg&format=json`)
-  console.log(newResponse.data)
+  return Object.values(productsByIds);
 };
 
-getAllProducts();
+const modifiedProductsData = async (products) => {
+  const { data } = await axios.post(
+    "https://api.apify.com/v2/datasets?token=" + token
+  );
+  const { id } = data.data;
+  await axios.post(
+    `https://api.apify.com/v2/datasets/${id}/items?token=`+ token,
+    products
+  );
+
+  const modifiedData = await axios.get(
+    `https://api.apify.com/v2/datasets/${id}/items?token=${token}`
+  );
+  console.log(modifiedData.data);
+};
+
+const main = async () => {
+  const products = await getCheapestProducts();
+  await modifiedProductsData(products)
+};
+
+main();
